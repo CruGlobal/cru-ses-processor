43c71dba7e0f963bbe9a4b7ed074242e7a5c07de
Brian Zoetewey <brian.zoetewey@cru.org>
2019-10-16 13:09:55 -0400

    Implement process-message lambda.

--- handlers/process-message.js
+++ handlers/process-message.js
@@ -5,6 +5,70 @@ import rollbar from '../config/rollbar'
 
 export const handler = async (lambdaEvent) => {
   try {
+    // Parse message from lambdaEvent
+    const message = JSON.parse(lambdaEvent.Records[0].Sns.Message)
+
+    // Create message attributes
+    const messageAttributes = {
+      // See https://docs.aws.amazon.com/ses/latest/DeveloperGuide/notification-contents.html
+      notificationType: {
+        DataType: 'String',
+        StringValue: message.notificationType
+      },
+      source: {
+        DataType: 'String',
+        StringValue: message['mail'].source
+      },
+      destination: {
+        DataType: 'String.Array',
+        StringValue: JSON.stringify(message['mail'].destination)
+      }
+    }
+    switch (message.notificationType) {
+      case 'Bounce':
+        // See https://docs.aws.amazon.com/ses/latest/DeveloperGuide/notification-contents.html#bounce-object
+        messageAttributes.bounceType = {
+          DataType: 'String',
+          StringValue: message['bounce'].bounceType
+        }
+        messageAttributes.bounceSubType = {
+          DataType: 'String',
+          StringValue: message['bounce'].bounceSubType
+        }
+        messageAttributes.bouncedRecipients = {
+          DataType: 'String.Array',
+          StringValue: JSON.stringify(message['bounce'].bouncedRecipients)
+        }
+        break
+      case 'Complaint':
+        // See https://docs.aws.amazon.com/ses/latest/DeveloperGuide/notification-contents.html#complaint-object
+        const complaints = message['complaint'].complainedRecipients.map(val => val.emailAddress)
+        messageAttributes.complainedRecipients = {
+          DataType: 'String.Array',
+          StringValue: JSON.stringify(complaints)
+        }
+        if (message['complaint'].complaintFeedbackType) {
+          messageAttributes.complaintFeedbackType = {
+            DataType: 'String',
+            StringValue: message['complaint'].complaintFeedbackType
+          }
+        }
+        break
+      case 'Delivery':
+        // See https://docs.aws.amazon.com/ses/latest/DeveloperGuide/notification-contents.html#delivery-object
+        message.recipients = {
+          DataType: 'String.Array',
+          StringValue: JSON.stringify(message['delivery'].recipients)
+        }
+        break
+    }
+
+    const sns = new SNS({ apiVersion: '2010-03-31' })
+    return await sns.publish({
+      TargetArn: process.env.SNS_SES_EVENTS_FILTERABLE_ARN,
+      Message: JSON.stringify(message),
+      MessageAttributes: messageAttributes
+    }).promise()
   } catch (error) {
     rollbar.error('process-message error', error)
     return Promise.reject(error)
