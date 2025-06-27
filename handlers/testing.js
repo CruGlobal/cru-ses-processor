5dd542ff5e84af3aa90b62c551c9056a5704b519
Brian Zoetewey <brian.zoetewey@cru.org>
2025-06-27 15:51:49 -0400

    Add some logging

--- handlers/testing.js
+++ handlers/testing.js
@@ -1,5 +1,6 @@
 export const handler = async (lambdaEvent) => {
   const envKeys = Object.keys(process.env)
   envKeys.sort()
+  console.log(envKeys)
   return Promise.resolve(JSON.stringify(envKeys, null, 2))
 }
